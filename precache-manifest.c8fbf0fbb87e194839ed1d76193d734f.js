43760ba46a0cf2a7b9d1aef017a2ec0d21538408
Fabio Carrella <fabiocarrella@gmail.com>
2020-05-05 12:05:10 +0200

    Updates

---
+++ precache-manifest.c8fbf0fbb87e194839ed1d76193d734f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9808395fa0fc3dac787bd92b905f8a",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "a56f3887ff1a2450b60f",
+    "url": "/battleship/static/css/main.e7808f93.chunk.css"
+  },
+  {
+    "revision": "e725c8eb7dfa004ceb13",
+    "url": "/battleship/static/js/2.d8dc0d8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.d8dc0d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a56f3887ff1a2450b60f",
+    "url": "/battleship/static/js/main.11782278.chunk.js"
+  },
+  {
+    "revision": "e56b4ff77b9077728859",
+    "url": "/battleship/static/js/runtime-main.2c6d9b56.js"
+  }
+]);
